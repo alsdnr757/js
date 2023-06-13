69f54733183defaa04d93781c86e5c193c956320
alsdnr757 <133009126+alsdnr757@users.noreply.github.com>
2023-06-13 12:00:30 +0000

    django 두번째수업

---
+++ sticky_memo/memo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+from memo.models import Memo
+# Create your views here.
+# sticky_memo/memo/views.py
+from django.shortcuts import render
+
+def get_all_memo(request):
+    memo_list = Memo.objects.all()
+    context = {"memos": memo_list}
+
+    return render(request, "memo_list.html", context)
